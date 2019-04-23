cbe59908589d24fc3b0b818cb2d9699fb4f5fd61
dzjymyll <yuqixiao2-c@my.cityu.edu.hk>
2019-04-23 17:16:54 +0800

    update

--- test.py
+++ test.py
@@ -39,11 +39,6 @@ print(model.input_shape)
 print(model.output_shape)
 model.add(MaxPooling1D(pool_size = (2), strides=(1)))
 print(model.output_shape)
-#model.add(keras.layers.core.Reshape([20,-1,1]))
-#print(model.output_shape)
-#model.add(Conv2D (kernel_size = (2,3), filters = 4, activation='relu'))
-#print(model.output_shape)
-#model.add(MaxPooling2D(pool_size = (1,2), strides=(1,2)))
 print(model.output_shape)
 model.add(Flatten())
 
@@ -57,11 +52,51 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer= Adam(), metrics=['accuracy'])
 model.summary()
 
-history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=128,
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=128,
                         epochs=20, verbose=1, shuffle=True)
 model.evaluate(X_test, Y_test, verbose=0)
-model_json = model.to_json()
-
 loss, acc = model.evaluate(X_test, Y_test, verbose=0)
 print('Test loss:', loss)
 print('Test accuracy:', acc)
+
+ynew = model.predict_proba(input.New)
+
+print('Class_1',ynew)
+
+
+##########################################class_2
+
+X_train = input.traindata_2
+Y_train = to_categorical(input.trainlabel_2)
+X_test = input.testdata_2
+Y_test = to_categorical(input.testlabel_2)
+X = input.train_input
+
+model_2 = Sequential()
+model_2.add(Conv1D (kernel_size = (3), filters = 2, input_shape=X.shape[1:], activation='relu'))
+
+model_2.add(MaxPooling1D(pool_size = (2), strides=(1)))
+
+model_2.add(Flatten())
+
+model_2.add(Dense(512, activation='sigmoid'))
+model_2.add(Dropout(0.5))
+model_2.add(Dense(2, activation='softmax'))
+
+sgd=SGD(lr=learning_rate)
+
+model_2.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer= Adam(), metrics=['accuracy'])
+model_2.summary()
+
+model_2.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=128,
+                        epochs=20, verbose=1, shuffle=True)
+model.evaluate(X_test, Y_test, verbose=0)
+loss, acc = model.evaluate(X_test, Y_test, verbose=0)
+print('Test loss:', loss)
+print('Test accuracy:', acc)
+
+ynew = model.predict_proba(input.New)
+
+print('Class_2',ynew)
+
